c2199e73b1f13a4852b7b3ba7e8067283679276c
Andriux <andriux959@gmail.com>
2016-11-04 16:19:55 +0000

    Improve code
    Can now parse and output 38.8% of the raw copypasta compared to the
    previous 30.6%.

--- morse/morse.ino
+++ morse/morse.ino
@@ -1,7 +1,8 @@
-String navySeal = "WHAT THE FUCK DID YOU JUST FUCKING SAY ABOUT ME   YOU LITTLE BITCH    ILL HAVE YOU KNOW I GRADUATED TOP OF MY CLASS IN THE NAVY SEALS   AND IVE BEEN INVOLVED IN NUMEROUS SECRET RAIDS ON AL-QUAEDA   AND I HAVE OVER 300 CONFIRMED KILLS    I AM TRAINED IN GORILLA WARFARE AND IM THE TOP SNIPER IN THE ENTIRE US ARMED FORCES    YOU ARE NOTHING TO ME BUT JUST ANOTHER TARGET   YOU GODDAMN IDIOT    I WILL SHIT FURY ALL OVER YOU AND YOU WILL DROWN IN IT    YOURE FUCKING DEAD   KIDDO   ";
+String navySeal = "What the fuck did you just fucking say about me, you little bitch? I’ll have you know I graduated top of my class in the Navy Seals, and I’ve been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills.  I am trained in gorilla warfare and I’m the top sniper in the entire US armed forces.  You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker.";
 
-char morseLetters[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-String morseCode[] = {".-","-...","-.-.", "-..",".","..-.", "--.", "....","..", ".---", "-.-",".-..","--","-.","---",".--.", "--.-", ".-.", "...", "-","..-","...-", ".--","-..-", "-.--", "--..", " "};
+char morseLettersLowercase[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+char morseLettersUppercase[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+String morseCode[] = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", " ", ""};
 
 int delayTime = 100;
 
@@ -10,29 +11,42 @@ void setup() {
 }
 
 void loop() {
-  delay(50 * delayTime); //initial 5s pause
-  /* for every character in the navy seal copypasta... */
+  /* Initial 2s pause */
+  delay(20 * delayTime);
+  
+  /* For every character in the navy seal copypasta... */
   for (int textIterator = 0; textIterator < navySeal.length(); textIterator++) {
     /* ...find corresponding character in morse alphabet */
-    int location = 26; //defaults to empty space
-    for (int characterIterator = 0; characterIterator < sizeof(morseLetters); characterIterator++) {
-      if (navySeal.charAt(textIterator) == morseLetters[characterIterator]) {
-        location = characterIterator; //sets location if character matches
+    int location = 27; //Defaults to ignoring character
+
+    /* Handles space bar */
+    if (navySeal.charAt(textIterator) == ' ') {
+      output(morseCode[26]);
+      continue;
+    }
+    
+    /* For every known morse code character... */
+    for (int characterIterator = 0; characterIterator < sizeof(morseLettersUppercase); characterIterator++) {
+      /* ...checks for both uppercase and lowercase version */
+      if (navySeal.charAt(textIterator) == morseLettersUppercase[characterIterator] || navySeal.charAt(textIterator) == morseLettersLowercase[characterIterator]) {
+        /* Saves location in case of match */
+        location = characterIterator;
         break;
       }
     }
-    /* outputs morse version of the letter */
+    
+    /* Outputs morse version of the letter */
     output(morseCode[location]);
     delay(4 * delayTime);
   }
-  
+
 }
 
 void output(String outputText) {
-  /* for every simbol in the morse code... */
-  for(int morseIterator = 0; morseIterator < outputText.length(); morseIterator++) {
-    /* ...do appropriate logic */
-    switch(outputText.charAt(morseIterator)) {
+  /* For every simbol in the morse code... */
+  for (int morseIterator = 0; morseIterator < outputText.length(); morseIterator++) {
+    /* ...do the correct output */
+    switch (outputText.charAt(morseIterator)) {
       case '.': shortBlink(); break;
       case '-': longBlink(); break;
       case ' ': pause(); break;
@@ -42,22 +56,22 @@ void output(String outputText) {
   }
 }
 
-/** outputs '.' with a short blink **/
+/** Outputs '.' with a short blink **/
 void shortBlink() {
   digitalWrite(LED_BUILTIN, HIGH);
   delay(delayTime);
   digitalWrite(LED_BUILTIN, LOW);
 }
 
-/** outputs '-' with a long blink **/
+/** Outputs '-' with a long blink **/
 void longBlink() {
   digitalWrite(LED_BUILTIN, HIGH);
   delay(4 * delayTime);
   digitalWrite(LED_BUILTIN, LOW);
 }
 
-/** outputs ' ' with a pause **/
+/** Outputs ' ' with a pause **/
 void pause() {
-  delay(2 * delayTime);
+  delay(4 * delayTime);
 }
 
